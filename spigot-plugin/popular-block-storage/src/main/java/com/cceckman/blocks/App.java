6991fa6c49a65c509192f2d3fe3e13d04bc81855
Charles Eckman <cceckman@google.com>
2020-03-28 11:25:29 -0700

    Initial commit of Spigot plugin

---
+++ spigot-plugin/popular-block-storage/src/main/java/com/cceckman/blocks/App.java
@@ -0,0 +1,13 @@
+package com.cceckman.blocks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
