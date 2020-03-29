cab0b574ba608a08588f203a0d33e30f9077efc7
Charles Eckman <cceckman@google.com>
2020-03-28 20:59:08 -0700

    Add a minimal client for network protocol

---
+++ get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import socket
+
+conn = socket.create_connection(("localhost", 4602))
+
+read_size = 20
+read_start = 4
+
+command = bytearray(1 + 8 + 4)
+
+command[0] = 0                                             # read
+command[1:9] = read_start.to_bytes(8, byteorder='little')  # at byte 4
+command[9:13] = read_size.to_bytes(4, byteorder='little')  # twenty bytes
+
+conn.send(command)
+
+resp = conn.recv(len(command) + read_size)
+print("Raw bytes: {}".format(resp))
+
+conn.close()
